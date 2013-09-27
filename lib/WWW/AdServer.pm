9d31ee32f80d4c7fe1585576cc253a0bd5e33d5e
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2013-09-27 06:13:08 +0000

    Import of SZABGAB/WWW-AdServer-1.01 from CPAN.
    gitpan-cpan-distribution: WWW-AdServer
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         SZABGAB/WWW-AdServer-1.01.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

--- lib/WWW/AdServer.pm
+++ lib/WWW/AdServer.pm
@@ -1,4 +1,7 @@
 package WWW::AdServer;
+{
+  $WWW::AdServer::VERSION = '1.01';
+}
 use Moo;
 #use MooX::late;
 
@@ -16,3 +19,27 @@ use WWW::AdServer::Database;
 
 1;
 
+__END__
+
+=pod
+
+=head1 NAME
+
+WWW::AdServer
+
+=head1 VERSION
+
+version 1.01
+
+=head1 AUTHOR
+
+Gabor Szabo <szabgab@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Gabor Szabo.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
--- lib/WWW/AdServer/Database/YAML.pm
+++ lib/WWW/AdServer/Database/YAML.pm
@@ -1,4 +1,7 @@
 package WWW::AdServer::Database::YAML;
+{
+  $WWW::AdServer::Database::YAML::VERSION = '1.01';
+}
 use Moo::Role;
 use 5.010;
 
@@ -71,3 +74,28 @@ sub get_ads {
 
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+WWW::AdServer::Database::YAML
+
+=head1 VERSION
+
+version 1.01
+
+=head1 AUTHOR
+
+Gabor Szabo <szabgab@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Gabor Szabo.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
